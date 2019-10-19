cd4a11c4df3b7bc8c49ec404975be5f751781b13
DongYue Sun <993307098@qq.com>
2019-10-19 22:56:36 +0800

    3 4 5

---
+++ ex3.py
@@ -0,0 +1,32 @@
+#打印“我将会清点鸡的种数：”
+print("I will now count my chickens:")
+#打印“母鸡 30.0” 30➗6=5.0 5.0+25=30.0
+print("Hens",25 + 30 / 6)
+#打印“公鸡 97”，25×3=75 75对4取余=3 100-3=97
+print("Roosters",100 - 25 * 3 % 4)
+
+#打印“现在我将清点鸡蛋：”
+print("Now I will count the eggs:")
+#打印“6.75” 4对2取余=0 1/4=0.25 3+2+1-5=1 1+0-0.25=0.75 0.75+6=6.75
+print(3 + 2 + 1 -5 +4 % 2 - 1 / 4 + 6)
+#打印“3 + 2 < 5 = 7 是真的吗？”
+print("Is it ture that 3 + 2 < 5 - 7?")
+#打印“False” 3+2=5 5-7=-2 5!< -2
+print(3 + 2 < 5 - 7)
+
+#打印“3+2等于多少？5”
+print("What is 3 + 2?", 3 + 2)
+#打印“5-7等于多少？-2”
+print("What is 5 - 7?", 5 - 7)
+#打印“Oh,这就是为什么为假了”
+print("Oh,that's why it's False.")
+
+#打印“更多的呢。”
+print("How about some more.")
+
+#打印“更大吗？Ture”
+print("Is it greater?", 5 > -2)
+#打印“大于或者等于吗？True”
+print("Is it greater or equal?", 5 >= -2)
+#打印”小于或者等于吗？False.”。
+print("Is it less or equal?", 5 <= -2)
