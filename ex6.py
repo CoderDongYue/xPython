f70cacb67cdbc01f2c433e2d2d6ac729ff2b1fbc
DongYue Sun <993307098@qq.com>
2019-10-20 22:49:16 +0800

    习题6 字符串和文本

---
+++ ex6.py
@@ -0,0 +1,35 @@
+#有10种类型的人
+types_of_people = 10
+#令x="有10种类型的人"
+x = f"There are {types_of_people} types_of_people."
+
+#定义变量binary
+binary = "binary"
+#定义变量do_not
+do_not = "don't"
+#令y=“那些知道有二进制种的人和那些不知道的人”
+y = f"Those who know {binary} and those who {do_not}"
+
+#打印x
+print(x)
+#打印y
+print(y)
+#打印“我说有10种类型的人”
+print(f"I said: {x}")
+#打印“我也说那些知道有二进制种的人和那些不知道的人”
+print(f"I also said: '{y}'")
+
+#定义变量hilarious
+hilarious = False
+#笑话评价
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+#打印变量
+print(joke_evaluation.format(hilarious))
+
+定义w e
+w = "This is the left side of..."
+e = "a string with a right side."
+
+#字符串相加
+print(w + e)
